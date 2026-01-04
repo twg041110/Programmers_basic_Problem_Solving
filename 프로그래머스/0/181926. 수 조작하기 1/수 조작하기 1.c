21930d2d46b65acf8e841903b1a06fe7d4cb657a
twg041110 <150268649+twg041110@users.noreply.github.com>
2026-01-04 23:44:11 +0900

    [level 0] Title: 수 조작하기 1, Time: 0.63 ms, Memory: 4.18 MB -BaekjoonHub

---
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+// 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
+int solution(int n, const char* control) {
+    for(int i = 0; i<strlen(control); i++){
+        if(control[i] == 'w'){
+            n += 1;
+        } else if(control[i] == 's'){
+            n -= 1;
+        } else if(control[i] == 'd'){
+            n += 10;
+        } else{
+            n -= 10;
+        }
+    }
+    return n;
+}
